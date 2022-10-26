2c28d6c187ac62bb475806dc416b67be01b068cb
Jessica Wilson <jessica.a.wilson99@gmail.com>
2022-10-26 13:06:41 -0500

    Completed create, about to start tests

---
+++ jailbreak/src/test/java/learn/jailbreak/domain/InventorySlotServiceTest.java
@@ -0,0 +1,12 @@
+package learn.jailbreak.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventorySlotServiceTest {
+
+    @Test
+    void create() {
+    }
+}
