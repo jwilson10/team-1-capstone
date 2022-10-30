f6505a1e00b3a2f4230cb2b1d2d8a52ed60b7c2a
Cruz Vega <cgvega2@gmail.com>
2022-10-30 13:54:41 -0600

    added new tables

--- jailbreak/src/data/schemas.sql
+++ jailbreak/src/data/schemas.sql
@@ -2,7 +2,7 @@ drop database if exists jailbreak;
 create database jailbreak;
 use jailbreak;
 
-create table role (
+create table `role` (
 	role_id int primary key auto_increment,
     role_name  varchar(100) not null
 );
@@ -16,43 +16,57 @@ create table `user` (
         unique(username),
 	constraint fk_user_role_id
 		foreign key (role_id)
-        references role(role_id)
+        references `role`(role_id)
     );
     
 
 
 create table game(
-game_id int primary key auto_increment,
-user_id int not null ,
-character_name varchar(100) not null,
-game_number int not null,
-constraint game_user_id
-    foreign key (user_id)
-    references `user`(user_id)
+	game_id int primary key auto_increment,
+	user_id int not null ,
+	character_name varchar(100) not null,
+	game_number int not null,
+	constraint fk_game_user_id
+		foreign key (user_id)
+		references `user`(user_id)
 );
 
 create table resources(
-resource_id int primary key auto_increment,
-resource_name varchar(100) not null ,
-resource_value int not null,
-resource_default_inc_rate int not null,
-constraint resources_resource_name
-    unique(resource_name)
+	resource_id int primary key auto_increment,
+	resource_name varchar(100) not null ,
+	resource_value int not null,
+	resource_default_inc_rate int not null,
+	constraint uq_resources_resource_name
+		unique(resource_name)
 );
 
 create table inventory_slot(
-slot_id int primary key auto_increment,
-game_id int not null,
-resource_id int not null,
-quantity int not null,
-constraint inventory_slot_resource_id
-    foreign key (resource_id)
-    references resources(resource_id),
-constraint invetory_slot_game_id
-    foreign key (game_id)
-    references game(game_id)
+	slot_id int primary key auto_increment,
+	game_id int not null,
+	resource_id int not null,
+	quantity int not null,
+	constraint fk_inventory_slot_resource_id
+		foreign key (resource_id)
+		references resources(resource_id),
+	constraint fk_inventory_slot_game_id
+		foreign key (game_id)
+		references game(game_id)
+);
+create table `event`(
+	event_id int primary key auto_increment,
+    event_name varchar(100) not null
 );
 
+create table game_event(
+	event_id int not null,
+    game_id int not null,
+	constraint fk_game_event_event_id
+		foreign key (event_id)
+        references `event`(event_id),
+	constraint fk_game_event_game_id
+        foreign key (game_id)
+        references inventory_slot(game_id)
+	);
 use jailbreak;
 select * from `user`;
 
