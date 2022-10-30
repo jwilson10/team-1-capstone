a79d0c93ec30c9be216b37e4f8cfac33bacf55fa
Jessica Wilson <jessica.a.wilson99@gmail.com>
2022-10-30 16:13:53 -0500

    Completed Event Service

--- jailbreak/src/main/java/learn/jailbreak/domain/EventService.java
+++ jailbreak/src/main/java/learn/jailbreak/domain/EventService.java
@@ -1,4 +1,23 @@
 package learn.jailbreak.domain;
 
+import learn.jailbreak.data.EventRepository;
+import learn.jailbreak.models.Event;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public Event findByEventName(String name){
+        return eventRepository.findByEventName(name);
+    }
+
+    public Event findByEventId(int eventId){
+        return eventRepository.findById(eventId).orElse(null);
+    }
 }
