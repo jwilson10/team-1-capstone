da5dd5db3c0bf086ae8076a9b4bec1d18c88a5d6
Jessica Wilson <jessica.a.wilson99@gmail.com>
2022-11-02 10:28:23 -0500

    Can generate a blank map; working now on generating walls

--- client/jailbreak/src/components/Map.js
+++ client/jailbreak/src/components/Map.js
@@ -1,9 +1,25 @@
 
 function Map(){
 
-return(<div> 
-    <h1>A map will go here</h1>
-    </div>);
+    let mapArr = new Array(10);
+
+    mapArr.push("#".repeat(60));
+    function generateMap(){
+        for (let i = 0; i < 15; i++) {
+        let newArr = new Array;
+            for(let j = 0; j < 60; j++){
+                newArr.push(" . ");                
+            }
+        mapArr.push(newArr);
+        }
+    }
+
+    generateMap();
+
+    return(<div> 
+        <h1>A map will go here</h1>
+        <div>{mapArr.map(row => <div class="d-grid gap-2 col-6 mx-auto">{row}</div>)}</div>
+        </div>);
 }
 
 export default Map;
